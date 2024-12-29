cab3f57cfc00b3081e5552f4c9ba8cfc76036a17
brijesh007 <uietbrijesh@gmail.com>
2024-12-29 18:41:02 +0530

    vercelcommit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,13 +4,6 @@ const jwt = require('jsonwebtoken');
 exports.registerUser = async (req, res) => {
   const { username, email, password } = req.body;
   try {
-    // Check if a user with the same email already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
-    }
-
-    // Create and save the new user
     const user = new User({ username, email, password });
     await user.save();
     res.status(201).json({ message: 'User registered successfully' });
